864daeaf3fdfbaaab0278a051ea79d48cbed7b46
Andre Ponce <ndr.hbk@gmail.com>
2025-05-03 18:18:10 +0800

    chore(spartan-ng): added spartan Angular component library

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
