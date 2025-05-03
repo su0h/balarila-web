6beb3dda003de6f14b1a6fb0b10048a7a67aaa98
Andre Ponce <ndr.hbk@gmail.com>
2025-05-03 17:09:55 +0800

    chore: nuked old files

--- src/assets/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BalarilaBG } from './balarila-bg.jpg';
-export { default as BalarilaLogo } from './balarila-logo.png';
--- src/components/pages/HomePage/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-import { Navbar } from '../../common';
-
-const HomePage: FC = () => {
-  return <Navbar />;
-};
-
-export default HomePage;
